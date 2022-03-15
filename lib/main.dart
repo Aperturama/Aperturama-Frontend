26e05e2007a1c99395d1f3722d28f262b6e01f22
hbh7 <hbh7@hbh7.com>
2022-03-15 00:51:46 -0400

    Navigation drawer

--- lib/main.dart
+++ lib/main.dart
@@ -141,6 +141,43 @@ class _HomePageState extends State<HomePage> {
         tooltip: 'Change Grid Size',
         child: Text(_gridSize.toString()),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+
+
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
